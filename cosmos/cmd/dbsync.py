0c01ddbbf9de916bab514d7c2e6a228f408eb4cb
vminger <vminger@163.com>
2019-02-09 20:47:46 +0800

    Add dbsync cmd.

---
+++ cosmos/cmd/dbsync.py
@@ -0,0 +1,81 @@
+# Copyright 2019 PandoraGo Co.,LTD.
+# All Rights Reserved.
+#
+# Tony 2019/2/9: Add dbsync cmd.
+
+"""
+Run storage database migration.
+"""
+
+import sys
+
+from oslo_config import cfg
+
+from cosmos.common.conf import CONF
+from cosmos.common.db import migration
+from cosmos.common.i18n import _
+from cosmos.common import service
+
+
+class DBCommand(object):
+
+    def upgrade(self):
+        migration.upgrade(CONF.command.revision)
+
+    def revision(self):
+        migration.revision(CONF.command.message, CONF.command.autogenerate)
+
+    def stamp(self):
+        migration.stamp(CONF.command.revision)
+
+    def version(self):
+        print(migration.version())
+
+    def create_schema(self):
+        migration.create_schema()
+
+
+def add_command_parsers(subparsers):
+    command_object = DBCommand()
+
+    parser = subparsers.add_parser(
+        'upgrade',
+        help=_("Upgrade the database schema to the latest version. "
+               "Optionally, use --revision to specify an alembic revision "
+               "string to upgrade to."))
+    parser.set_defaults(func=command_object.upgrade)
+    parser.add_argument('--revision', nargs='?')
+
+    parser = subparsers.add_parser('stamp')
+    parser.add_argument('--revision', nargs='?')
+    parser.set_defaults(func=command_object.stamp)
+
+    parser = subparsers.add_parser(
+        'revision',
+        help=_("Create a new alembic revision. "
+               "Use --message to set the message string."))
+    parser.add_argument('-m', '--message')
+    parser.add_argument('--autogenerate', action='store_true')
+    parser.set_defaults(func=command_object.revision)
+
+    parser = subparsers.add_parser(
+        'version',
+        help=_("Print the current version information and exit."))
+    parser.set_defaults(func=command_object.version)
+
+    parser = subparsers.add_parser(
+        'create_schema',
+        help=_("Create the database schema."))
+    parser.set_defaults(func=command_object.create_schema)
+
+
+def main():
+    command_opt = cfg.SubCommandOpt('command',
+                                    title='Command',
+                                    help=_('Available commands'),
+                                    handler=add_command_parsers)
+
+    CONF.register_cli_opt(command_opt)
+
+    service.prepare_service(sys.argv)
+    CONF.command.func()
